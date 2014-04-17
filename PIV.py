03261866730ef67a3daee22181ea5d70cf86c186
LTChaseJohnson <chasevjohnson@gmail.com>
2014-04-17 16:26:04 -0400

    Working on capturing more than one particle

--- PIV.py
+++ PIV.py
@@ -9,7 +9,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 from math import *
 from matplotlib.patches import Polygon
-from scipy.integrate import quad
+from scipy import integrate
 import scipy.special as sp
 
 #Os = input('Enter particle size (microns): ')
@@ -28,6 +28,8 @@ lamb = lam/(10**9)
 #do = input('Enter object distance (mm): ')
 do = 210.
 U = input('Enter flow speed (m/s): ')
+alphad = input('Enter flow angle: ')
+alpha = alphad*pi/180
 beta = np.sqrt(3.67)
 
 # Sensor Size in microns
@@ -57,20 +59,42 @@ print ('Recommended images size (microns): '),pix*2.5
 dte = Is/(3.*Ui)
 print ('Recommended exposure time (us): '),dte
 Es = input('Enter exposure time (us): ')
-
+ 
 # Sampling time based on streak time across 3 pixels
 tsample = (pix + ((1/3.)*Is))/Ui
 fps = (1/tsample)*10**6
 print ('Recommended sampling time (us): '),tsample
 print ('Recommended sampling frequency (fps): '),fps
 
+def I(x,beta,ds,a,b):
+    def func(s):
+        return np.exp(-4*beta**2*x**2/ds**2)
+    return integrate.quad(lambda s:func(s),a,b)[0]
+
+xp = -50*np.random.random(5)
+yp = -5*np.random.random(5)
+plt.figure(1)
+plt.scatter(xp,yp)
+
 N = 100                                  #Number of frames to expose
 rp = 0.5*Is
 t = np.linspace(0,N,N+1)                 #Time step for each frame
-for i in range(N):
-    x = 
+E = np.empty_like(t)                     #Empty exposure array
 
-'''# Assuming the leading edge of a particle is aligned with the trailing
+for i in range(5):
+    for j in range(N):
+        x = xp[i]+cos(alpha)*Ui*t[j]
+        if (x+rp <0) or (x-rp >0):
+            E[j]=0
+        else: E[j]=I(x,beta,ds,x,x+Ui*Es)
+
+plt.figure(2)
+plt.bar(t,E)
+plt.xlabel('Time Steps')
+plt.ylabel('Exposure')
+plt.xlim(min(t),max(t))
+
+"""# Assuming the leading edge of a particle is aligned with the trailing
 # edge of the pixel, the pixel exposure will be:
 
 def Intensity(x):
@@ -111,5 +135,6 @@ plt.title('Exposure Distribution of Sequentially Fired Pixels')
 plt.ylabel('Exposure Based on Normalized Intensity Profile')
 cell = np.linspace(1,3,3)
 Ep = [Ep1,Ep2,Ep3]
-plt.bar(-cell,Ep)'''
+plt.bar(-cell,Ep)"""
+
 plt.show()
